3492e656372c3881f7afc91df46120631e9d4b23
DaveCorleto <davide.corleto@fastwebnet.it>
2024-02-26 16:45:38 +0100

    import model in seeder...

--- database/seeders/TechnologiesSeeder.php
+++ database/seeders/TechnologiesSeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Technology;
+use App\Models\Project;
+use App\Models\Type;
+
+
+
 class Technologies extends Seeder
 {
     /**
